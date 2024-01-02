409afd7ee41c99a5a57746b7a14f85554c8cd083
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-02 23:51:48 +0530

    Strings in JS

---
+++ 01_Data_Types/String.js
@@ -0,0 +1,10 @@
+const name = "Zahid";
+const score = 100;
+
+console.log(name + score)
+
+// Modern Syntax
+
+console.log(`Hello, My name is ${name} and I scored ${score}`);
+
+const gameName = new String("combat");

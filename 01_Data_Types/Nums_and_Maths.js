2a46f84b9754ad824d306034512a9f0c40a58dcf
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-04 10:52:02 +0530

    Date and Time in JS

--- 01_Data_Types/Nums_and_Maths.js
+++ 01_Data_Types/Nums_and_Maths.js
@@ -3,7 +3,7 @@ const score = 400;
 const balance = new Number(100);
 
 // console.log(score);
-// console.log(balance); 
+// console.log(balance);
 
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(2));
@@ -31,9 +31,9 @@ const hundreds = 1000000;
 
 console.log(Math.random());
 console.log(Math.random() * 10);
-console.log((Math.random() * 10) + 1)
+console.log(Math.random() * 10 + 1);
 
 const min = 10;
 const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);

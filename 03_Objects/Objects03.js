e072b7e7334a21d7cdd937e21c1faf067489bbae
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-14 23:37:18 +0530

    Object destructuring in JS

--- 03_Objects/Objects03.js
+++ 03_Objects/Objects03.js
@@ -18,6 +18,58 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userFullName.firstName)
+// console.log(regularUser.fullname?.userFullName.firstName)
 
-// Objects are most important features in JS
+// combinig objects
+
+const obj1 = {
+    1:"a",2:"b"
+}
+const obj2 = {
+    3:"c",4:"d"
+}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+// const obj3 = {...obj1,...obj2}
+
+// console.log(obj3)
+
+const users = [{
+    id:1,
+    email:"i@zmail.com"
+},
+{
+    id:2,
+    email:"j@zmail.com"
+},
+{
+    id:3,
+    email:"k@zmail.com"
+}]
+
+// console.log(users[0].email)
+
+// console.log(instaUser)
+
+// console.log(Object.keys(instaUser))
+// console.log(Object.values(instaUser))
+// console.log(Object.entries(instaUser))
+
+// console.log(instaUser.hasOwnProperty("isLoggedIN"))
+// console.log(instaUser.hasOwnProperty("isLoggedOUT"))
+
+// Destructuring of Objects
+
+const course = {
+    courseName: "English",
+    price:9999,
+    courseInstructor: "Zahid"
+}
+
+// console.log(course.courseInstructor)
+
+// const {courseName} = course;
+const {courseName:cName}= course
+
+console.log(cName)

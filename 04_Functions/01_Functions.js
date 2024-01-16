8e3d2c53828e0007cc28edc802f040e435ca69ff
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-16 23:30:00 +0530

    Function in JS 2

--- 04_Functions/01_Functions.js
+++ 04_Functions/01_Functions.js
@@ -54,3 +54,46 @@ function loginUserMessage(username = "Slave") {
 }
 
 //   console.log(loginUserMessage());
+
+function calculateCartPrice(num1) {
+  return num1;
+}
+
+console.log(calculateCartPrice(20, 30, 40));
+
+function calculateCartPrice2(...num) {
+  return num;
+}
+
+console.log(calculateCartPrice2(20, 30, 40));
+
+function calculateCartPrice3(val1, val2, ...num) {
+  return num;
+}
+
+console.log(calculateCartPrice3(20, 30, 40, 50, 100, 2000));
+
+const user = {
+  username: "Zahid",
+  score: 200,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `The name of user is ${anyObject.username} and score is ${anyObject.score}`
+  );
+}
+
+handleObject(user);
+handleObject({
+  username: "King",
+  score: "500",
+});
+
+const newArray = [100, 200, 300, 500, 1000];
+
+function getSecondElement(getArray) {
+  return getArray[1];
+}
+console.log(getSecondElement(newArray));
+console.log(getSecondElement([111, 2222, 3333, 4444, 55555, 0000]));

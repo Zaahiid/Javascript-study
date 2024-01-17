cf98249e980d743edee193af178164af16c97ef6
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-17 23:32:56 +0530

    Scope level and mini hoisting

--- 04_Functions/02_Scope.js
+++ 04_Functions/02_Scope.js
@@ -1,14 +1,48 @@
-
 let a = 300;
 
 if (true) {
   let a = 10;
   const b = 20;
   var c = 30;
-  console.log("INNER : ",a)
+  console.log("INNER : ", a);
 }
 
 /* console.log(a) */
 /* console.log(b) */
 // console.log(c);
-console.log(a)
+// console.log(a)
+
+function one() {
+  const username = "Zahid";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  console.log(website);
+  two();
+}
+one();
+
+if (true) {
+  const username = "Zahid";
+  if (username === "Zahid") {
+    const website = " Youtube";
+    console.log(username + website);
+  }
+  console.log(website);
+}
+console.log(username);
+
+// ++++++++++++++++ Interesting +++++++++++++++++++++++++++ //
+
+addOne(5);
+
+function addOne(num) {
+  return num + 1;
+}
+
+
+addTwo(5);
+const addTwo = function (num) {
+  return num + 2;
+};

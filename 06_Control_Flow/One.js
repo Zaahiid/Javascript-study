062ff4337648915df0a18f38d29704a3ee486ffb
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-20 23:28:12 +0530

    Control Flow in JS if and if else

---
+++ 06_Control_Flow/One.js
@@ -0,0 +1,17 @@
+const temperature = 60;
+
+if (temperature < 50) {
+  console.log("Temperature is less than 50deg");
+} else {
+  console.log("Temperature is greater than 50deg");
+}
+
+// Comparison operators
+// <, >, <=, >=, ==, !=, ===, !==
+
+const balance = 1000;
+if (balance > 500) console.log("Balance is greater than 500");
+// Implicit Scope
+
+
+

14b279f576951601e30db17a35a10ab2de8eeb31
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-21 00:30:48 +0530

    Ternary Operator

--- 06_Control_Flow/TruthyFalsy.js
+++ 06_Control_Flow/TruthyFalsy.js
@@ -23,7 +23,6 @@ if (Object.keys(emptyObj).length === 0) {
   console.log("Object is Empty");
 }
 
-
 // Nullish Coalescing Operator (??)
 let val1;
 
@@ -34,3 +33,9 @@ let val1;
 val1 = null ?? undefined ?? 30;
 
 console.log(val1);
+
+// Ternary operator
+
+const coffeePrice = 80;
+
+coffeePrice <= 100 ? console.log("Coffee is cheap") : console.log("Coffee is expensive");

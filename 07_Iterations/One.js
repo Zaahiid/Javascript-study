c05b223d3853ce88f645b3f2d592eb27f0d557cf
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-21 22:47:42 +0530

    Break and Continue

--- 07_Iterations/One.js
+++ 07_Iterations/One.js
@@ -35,3 +35,21 @@ for (let i = 0; i < myArray.length; i++) {
   const element = myArray[i];
   console.log(element);
 }
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 5) {
+    console.log(`Detected 5`);
+    break;
+  }
+  console.log(`value of i is ${i}`);
+}
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 5) {
+    console.log(`Detected 5`);
+    continue;
+  }
+  console.log(`value of i is ${i}`);
+}

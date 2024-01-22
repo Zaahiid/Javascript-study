04d0461e5ff6059a2fad7c683027860eb55224a0
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-22 23:06:11 +0530

    do while loop in JS

--- 07_Iterations/Two.js
+++ 07_Iterations/Two.js
@@ -14,3 +14,24 @@ while (arr < myArray.length) {
   console.log(`The value is ${myArray[arr]}`);
   arr = arr + 1;
 }
+
+
+// do while loop
+
+let score =0;
+
+do{
+  console.log(`The value is ${score}`);
+  score++;
+  
+}while(score <= 10)
+
+let marks =11;
+
+do{
+  console.log(`The value is ${marks}`);
+  marks++;
+  
+}while(marks <= 10)
+
+// The do-while loop works by first executing the code block inside the do statement and then checking the specified condition in the while statement. If the condition is true, the loop will continue to iterate; if it's false, the loop will exit.ٖ

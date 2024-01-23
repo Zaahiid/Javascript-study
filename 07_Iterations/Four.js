8bdedb6f52ed1386bd1d072d7323c5b131218aa8
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-23 10:09:41 +0530

    for...in loop with Map

--- 07_Iterations/Four.js
+++ 07_Iterations/Four.js
@@ -20,3 +20,17 @@ for (const key in programming) {
 for (const key in programming) {
   console.log(programming[key]);
 }
+
+// for...in loop with Map
+
+const map = new Map();
+map.set("IN", "India");
+map.set("SA", "Saudi Arabia");
+map.set("EN", "England");
+map.set("IN", "India");
+
+for (const key in map) {
+  console.log(key);
+}
+
+// Program did not output anything!

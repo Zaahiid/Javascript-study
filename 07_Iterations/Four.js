09780be994ee11e56a8a6888bb33f62e2a123cdf
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-23 10:05:58 +0530

    for...in loop with Array

--- 07_Iterations/Four.js
+++ 07_Iterations/Four.js
@@ -8,3 +8,15 @@ const myObject = {
 for (const key in myObject) {
   console.log(`${key} is abbreviation for ${myObject[key]}`);
 }
+
+// for...in with Array
+
+const programming = ["JS", "RB", "PY", "JAVA"];
+
+for (const key in programming) {
+  console.log(key);
+}
+
+for (const key in programming) {
+  console.log(programming[key]);
+}

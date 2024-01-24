ed6cfec5c8920fedab27e7b3a5f1d9a11011475c
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-24 23:08:39 +0530

    using for...each fir returning an array

--- 07_Iterations/Six.js
+++ 07_Iterations/Six.js
@@ -15,3 +15,17 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const newNums = myNums.filter((nums) => nums > 4);
 
 console.log(newNums);
+
+// using for...each to return an array
+
+const myNums2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNums2 = [];
+
+myNums2.forEach((nums) => {
+  if (nums > 4) {
+    newNums2.push(nums);
+  }
+});
+
+console.log(newNums2);

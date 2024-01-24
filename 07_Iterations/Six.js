ae92ea98dd4dbbeee005080d0c41c0e31c4d50e5
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-24 23:24:13 +0530

    filter with an array of objects

--- 07_Iterations/Six.js
+++ 07_Iterations/Six.js
@@ -28,4 +28,104 @@ myNums2.forEach((nums) => {
   }
 });
 
-console.log(newNums2);
+// console.log(newNums2);
+
+
+// filter with an array of objects
+
+const books = [
+    {
+      title: "The Great Gatsby",
+      genre: "Novel",
+      publishYear: 1925,
+      editionYear: 2004  
+    },
+    {
+      title: "The Odyssey",
+      genre: "Epic Poem",
+      publishYear: -800, 
+      editionYear: 2021
+    },
+    {
+      title: "The Art of Computer Programming",
+      genre: "Computer Science",
+      publishYear: 1968,
+      editionYear: 2000
+    },
+    {
+      title: "The Republic",
+      genre: "Philosophy",
+      publishYear: -380,
+      editionYear: 2022
+    },
+    {
+      title: "A Tale of Two Cities",
+      genre: "Novel",
+      publishYear: 1859,
+      editionYear: 2022
+    },
+    {
+      title: "Leaves of Grass",
+      genre: "Poetry",
+      publishYear: 1855,
+      editionYear: 2005
+    },
+    {
+      title: "Anne of Green Gables",
+      genre: "Novel",
+      publishYear: 1908,
+      editionYear: 2022
+    },
+    {
+      title: "The Time Machine",
+      genre: "Science Fiction",
+      publishYear: 1895,
+      editionYear: 2022
+    },
+    {
+      title: "The Prince",
+      genre: "Philosophy",
+      publishYear: 1532,
+      editionYear: 2005
+    }, 
+    {
+      title: "The Trial",
+      genre: "Novel",
+      publishYear: 1925,
+      editionYear: 2022
+    },
+    {
+      title: "Gulliver's Travels", 
+      genre: "Satire",
+      publishYear: 1726,
+      editionYear: 2010
+    },
+    {
+      title: "The Divine Comedy",
+      genre: "Epic Poem", 
+      publishYear: 1320,
+      editionYear: 2022
+    },
+    {
+      title: "To Kill a Mockingbird",
+      genre: "Novel",
+      publishYear: 1960,
+      editionYear: 2022
+    },
+    {
+      title: "The Histories", 
+      genre: "History",
+      publishYear: -440,
+      editionYear: 1998
+    }
+  ];
+  
+  const novels = books.filter((book)=> book.genre === "Novel");
+  
+  console.log(novels)
+
+  const userBooks = books.filter((book)=>{
+    return book.genre === "Novel" && book.publishYear >= 1960
+  });
+  
+  console.log(userBooks)

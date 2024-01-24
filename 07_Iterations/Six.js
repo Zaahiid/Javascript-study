924b766ff209067511bf829cc653ee0c6e98650d
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-24 23:01:42 +0530

    filter loop in JS

---
+++ 07_Iterations/Six.js
@@ -0,0 +1,17 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// const values =coding.forEach(item => {
+//     return item;
+// });
+
+// console.log(values);
+
+// for...each returns undefined
+
+// filter loop
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNums = myNums.filter((nums) => nums > 4);
+
+console.log(newNums);

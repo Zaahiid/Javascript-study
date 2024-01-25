ed9b5293ade8a8641da3aaae458cb0496947045a
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-25 23:35:54 +0530

    reduce with arrow function example

--- 07_Iterations/Eight.js
+++ 07_Iterations/Eight.js
@@ -2,9 +2,12 @@
 
 const myNumbers = [1, 2, 3];
 
-const newNums = myNumbers.reduce(function (accumualtor, currentValue) {
-  console.log(`acc : ${accumualtor} and curVal : ${currentValue}`);
-  return accumualtor + currentValue;
+const newNums = myNumbers.reduce(function (accumulator, currentValue) {
+  console.log(`acc : ${accumulator} and curVal : ${currentValue}`);
+  return accumulator + currentValue;
 }, 0);
 
 console.log(newNums);
+
+const myTotal = myNumbers.reduce((acc,cur)=> acc + cur,0);
+console.log(myTotal)

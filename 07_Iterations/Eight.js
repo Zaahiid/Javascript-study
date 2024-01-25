f9368df73395d74f2dcc7eaae7a75632ddc70f05
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-25 23:45:14 +0530

    reduce with an array of objects

--- 07_Iterations/Eight.js
+++ 07_Iterations/Eight.js
@@ -11,3 +11,29 @@ console.log(newNums);
 
 const myTotal = myNumbers.reduce((acc,cur)=> acc + cur,0);
 console.log(myTotal)
+
+const shoppingCart = [
+  
+    {
+      itemName:"JS course",
+      price: 2999
+    },
+    {
+      itemName:"Python course",
+      price: 2999
+    },
+    {
+      itemName:"Mobile app development",
+      price: 5999
+    },
+    {
+      itemName:"DS course",
+      price: 12999
+    },
+    ];
+    
+    const totalAmount = shoppingCart.map((item)=>item.price).reduce((acc,cur)=>acc+cur,0);
+    const totalAmount2 = shoppingCart.reduce((acc,cur)=>acc+cur.price,0);
+    
+    console.log(totalAmount2)
+    console.log(totalAmount)
